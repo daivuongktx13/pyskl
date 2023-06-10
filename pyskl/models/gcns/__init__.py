11b53250c7435e477f5e8c0840648ac95faff995
Trung <trung.ct190098@sis.hust.edu.vn>
2023-06-10 12:03:49 +0700

    Add SHiftGCN

--- pyskl/models/gcns/__init__.py
+++ pyskl/models/gcns/__init__.py
@@ -5,3 +5,4 @@ from .msg3d import MSG3D  # noqa: F401, F403
 from .sgn import SGN  # noqa: F401, F403
 from .stgcn import STGCN  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
+from .shiftgcn import SHIFTGCN
