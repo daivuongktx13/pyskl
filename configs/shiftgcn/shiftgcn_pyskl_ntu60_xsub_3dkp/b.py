6b42d7b562a0d4d660cff95c4b06ae9c259f65fd
Trung <trung.ct190098@sis.hust.edu.vn>
2023-06-14 00:20:07 +0700

    JB 80epochs lr 0.01

--- configs/shiftgcn/shiftgcn_pyskl_ntu60_xsub_3dkp/b.py
+++ configs/shiftgcn/shiftgcn_pyskl_ntu60_xsub_3dkp/b.py
@@ -46,7 +46,7 @@ data = dict(
     test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='xsub_val'))
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005, nesterov=True)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
