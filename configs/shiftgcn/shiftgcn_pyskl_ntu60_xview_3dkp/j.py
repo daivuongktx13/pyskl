8948c07f0c0b411c42d95827d62d2b6605a9787c
trungct <trungct14@ghtk.co>
2023-06-26 20:06:49 +0700

    Half 80 epochs

--- configs/shiftgcn/shiftgcn_pyskl_ntu60_xview_3dkp/j.py
+++ configs/shiftgcn/shiftgcn_pyskl_ntu60_xview_3dkp/j.py
@@ -3,7 +3,7 @@ model = dict(
     backbone=dict(
         type='SHIFTGCN',
         graph_cfg=dict(layout='nturgb+d', mode='spatial'),
-        spatial_shift_graph='global'),
+        spatial_shift_graph='half'),
     cls_head=dict(type='GCNHead', num_classes=60, in_channels=256))
 
 dataset_type = 'PoseDataset'
