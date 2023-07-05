4b4521defa647b7d2d2a18ab86c5b0ee66d8560e
trungct <trungct14@ghtk.co>
2023-07-05 07:49:50 +0700

    80epochs 3/4

--- configs/shiftgcn/shiftgcn_pyskl_ntu60_xsub_3dkp/j.py
+++ configs/shiftgcn/shiftgcn_pyskl_ntu60_xsub_3dkp/j.py
@@ -51,7 +51,7 @@ optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005, nestero
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
-total_epochs = 28
+total_epochs = 16
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy'])
 log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
